f4ce4749fbf2b3077e7bb3cb9cce027c30f57d93
JonAEllis <bomb_kmofir@hotmail.com>
2016-08-03 15:09:05 -0700

    Add files via upload

---
+++ 7-27.js
@@ -0,0 +1,9 @@
+function movie(title, mpaa, directors, producers, studio, releaseDate) {
+    this.title = title;
+	this.mpaa = mpaa;
+	this.directors = directors;
+	this.producers = producers;
+	this.studio = studio;
+	this.releaseDate = releaseDate;
+}
+
